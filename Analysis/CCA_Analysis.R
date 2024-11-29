796b155e02010d8ca54b20476cf73a8eaecf3005
colemanferrell2 <caferr@unc.edu>
2024-11-28 23:41:24 -0500

    Format Traits Data

--- Analysis/CCA_Analysis.R
+++ Analysis/CCA_Analysis.R
@@ -3,10 +3,14 @@
 
 
 #add comment to talk about setting working directory
-
+# Set your working directory to the project folder
+setwd("C:/Users/colem/Desktop/STOR 674/STOR674 Final Project")
 
 library(combootcca)
-library(R.matlab)
 
-#Load MatLab Data file with Pairwise Correlations
+
+#Load .csv Data file with Pairwise Correlations and traits
 Pairwise_Corr_FC <- read.csv("Analysis/Pairwise_Corr_FC.csv")
+Subj_175_Traits <- read.csv("Analysis/Traits175.csv")
+Subj_confound_var <- read.csv("Analysis/Confounding_variables.csv")
+
--- Data Formatting/Formatting_Data.R
+++ Data Formatting/Formatting_Data.R
@@ -1,16 +1,17 @@
+#Load Libraries
 library(R.matlab)
 library(data.table)
 library(purrr)
 
-# Set your working directory to the project folder
+# Set your working directory to the project folder (an example is provided below)
 setwd("C:/Users/colem/Desktop/STOR 674/STOR674 Final Project")
+
+##### Functional Connectivity Correlation Matrix to Pairwise Correlation Matrix #####
 
 # Load Functional Connectivity Correlation Matrix from the data directory
 FC_Data <- readMat("Data/FC/HCP_cortical_DesikanAtlas_FC.mat") 
 list2env(FC_Data,envir = .GlobalEnv)
 hcp.cortical.fc <-flatten(hcp.cortical.fc)
-
-### Transforming Correlation Matrix into usable format ###
 
 # Removing subjects with empty entries
 keep <- sapply(hcp.cortical.fc, function(x) length(x) > 0)
@@ -44,4 +45,30 @@ final_data <- cbind(Subject_ID = subj.list.Filtered, pairwise_data)
 
 # Write to CSV for analysis into Analysis folder
 write.csv(final_data, "Analysis/Pairwise_Corr_FC.csv", row.names = FALSE)
-s
+
+##### 175 Traits #####
+
+# Load 175 traits from the data directory
+Traits_Data <- readMat("Data/traits/175traits/HCP_175Traits.mat") 
+list2env(Traits_Data,envir = .GlobalEnv)
+
+#Combine subject ID with trait responses, also rename column names
+traits.final <- cbind(hcp.subj.id,t(traits.175))
+colnames(traits.final)[2:ncol(traits.final)] <- paste0("Trait_", 1:(ncol(traits.final) - 1))
+colnames(traits.final)[1] <- "Subject_ID"
+
+#Write to .csv file in the /analysis folder
+write.csv(traits.final, "Analysis/Traits175.csv", row.names = FALSE)
+
+##### Confounding Variables (Age and Gender) #####
+
+# Load confounding MatLab file from the data directory
+confound_var <- readMat("Data/traits/age_gender_subjectid.mat") 
+list2env(confound_var,envir = .GlobalEnv)
+
+#Combine subject ID with variables, also rename column names
+confound_var_final <- cbind(allsubject.id,t(confond.variable))
+colnames(confound_var_final) <- c("Subject_ID","Age","Gender")
+
+#Write to .csv file in the /analysis folder
+write.csv(confound_var_final, "Analysis/Confounding_variables.csv", row.names = FALSE)
