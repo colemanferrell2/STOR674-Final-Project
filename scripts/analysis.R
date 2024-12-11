57b99dd43c047e9380fd9c2bd76d8902262cc3a2
colemanferrell2 <caferr@unc.edu>
2024-12-11 01:12:40 -0500

    Organize analysis R files

--- scripts/analysis.R
+++ scripts/analysis.R
@@ -1,57 +1,33 @@
-##### Setup #####
-# Install necessary packages
-# devtools::install_github("dankessler/combootcca")
-
-# Load Libraries
-library(combootcca)
-library(dplyr)
-library(ggplot2)
-library(CCA)
-
-# Set working directory and seed
+# Main script execution
 setwd("C:/Users/colem/Desktop/STOR 674/STOR674 Final Project")
-set.seed(123)
-
-# Load Data
 load("data/Sample_Data.RData")
+source("scripts/functions.R")
+setup_environment()
 
-# Source Functions
-source("code/functions.R")
+cca_results <- run_cca(PrincComp_FC, Synthetic_Subj_Traits)
+x_data <- extract_and_prepare_ci(cca_results$bootstrp_solution, "xcoef", c(1, 2))
+y_data <- extract_and_prepare_ci(cca_results$bootstrp_solution, "ycoef", c(1, 2))
 
-##### Statistical Inference Using combootcca #####
-# Create reference solution and bootstrap results
-ref_solution <- cc(PrincComp_FC, Synthetic_Subj_Traits)
-bootstrp_solution <- run_combootcca(PrincComp_FC, Synthetic_Subj_Traits, ref_solution)
-
-##### Extract Principal Component Confidence Intervals #####
-# Process and visualize confidence intervals for X
-all_PC_data_x <- process_principal_components(
-  bootstrp_solution$basic$xcoef,
-  c("\u03B21", "\u03B22")
+plot_ci(x_data, "Confidence Interval for Canonical Directions (X)", "results/Figures/CI_3PC_B.png")
+plot_ci(y_data, "Confidence Interval for Canonical Directions (Y)", "results/Figures/CI_3PC_G.png", y_limits = c(-8, 8))
+non_zero_x <- list(
+  First_Direction = x_data$Index[!x_data$CI_contains_zero & x_data$PC == "\u03B21"],
+  Second_Direction = x_data$Index[!x_data$CI_contains_zero & x_data$PC == "\u03B22"]
 )
-plot_confidence_intervals(all_PC_data_x, "output/Figures/CI_3PC_B.png", "Confidence Interval for First Three Canonical Directions (B)")
-
-# Process and visualize confidence intervals for Y
-all_PC_data_y <- process_principal_components(
-  bootstrp_solution$basic$ycoef,
-  c("\u03B31", "\u03B32")
+non_zero_y <- list(
+  First_Direction = y_data$Index[!y_data$CI_contains_zero & y_data$PC == "\u03B31"],
+  Second_Direction = y_data$Index[!y_data$CI_contains_zero & y_data$PC == "\u03B32"]
 )
-plot_confidence_intervals(all_PC_data_y, "output/Figures/CI_3PC_G.png", "Confidence Interval for First Three Canonical Directions (G)",ylim_range = c(-8, 8))
 
-# Add these after constructing x_first_PC, x_second_PC, etc.
-non_zero_indexes_first_PC_X <- which(!(x_first_PC$CI_contains_zero))
-non_zero_indexes_second_PC_X <- which(!(x_second_PC$CI_contains_zero))
+Loadings_Dir1 <- calculate_contributions(loadingsX_synthetic, non_zero_x$First_Direction)
+Loadings_Dir2 <- calculate_contributions(loadingsX_synthetic, non_zero_x$Second_Direction)
 
-non_zero_indexes_first_PC_Y <- which(!(y_first_PC$CI_contains_zero))
-non_zero_indexes_second_PC_Y <- which(!(y_second_PC$CI_contains_zero))
+library(DT)
 
-##### Canonical Loadings and Cross Loadings #####
-loading_results <- calculate_loadings(ref_solution, non_zero_indexes_first_PC_X, non_zero_indexes_second_PC_X, non_zero_indexes_first_PC_Y, non_zero_indexes_second_PC_Y)
+# Save as an interactive HTML table
+datatable(as.matrix(non_zero_x),colnames = "Significant PC Number") %>% saveWidget("results/Analysis/Significant Variable Weights/Significant_X_PC.html")
+datatable(as.matrix(non_zero_y),colnames = "Significant Trait Number") %>% saveWidget("results/Analysis/Significant Variable Weights/Significant_Y_Trait.html")
+datatable(Loadings_Dir1,rownames = NULL,options = list(order = list(list(1,'desc')))) %>% saveWidget("results/Analysis/PC Loadings/Region_Loadings_First_Direction.html")
+datatable(Loadings_Dir2,rownames = NULL,options = list(order = list(list(1,'desc')))) %>% saveWidget("results/Analysis/PC Loadings/Region_Loadings_Second_Direction.html")
 
-##### Principal Component Contributions #####
-result1 <- calculate_contributions(loadingsX, non_zero_indexes_first_PC_X)
-result2 <- calculate_contributions(loadingsX, non_zero_indexes_second_PC_X)
-
-print(result1)
-print(result2)
 
--- scripts/functions.R
+++ scripts/functions.R
@@ -1,5 +1,23 @@
-run_combootcca <- function(X, Y, ref_solution, level = 0.9, nboots = 1000) {
-  cca_ci_boot(
+# Load necessary libraries and source functions
+setup_environment <- function() {
+  library(combootcca)
+  library(dplyr)
+  library(ggplot2)
+  library(CCA)
+  source("scripts/functions.R")
+  set.seed(123)
+}
+
+# Load data and set working directory
+load_data <- function() {
+  setwd("C:/Users/colem/Desktop/STOR 674/STOR674 Final Project")
+  load("data/Sample_Data.RData")
+}
+
+# Perform Canonical Correlation Analysis
+run_cca <- function(X, Y, nboots = 1000, level = 0.9) {
+  ref_solution <- cc(X, Y)
+  bootstrp_solution <- cca_ci_boot(
     X, Y,
     align = combootcca:::cca_align_hungarian_weighted,
     ref = ref_solution,
@@ -7,62 +25,48 @@ run_combootcca <- function(X, Y, ref_solution, level = 0.9, nboots = 1000) {
     nboots = nboots,
     boot_type = "basic"
   )
+  list(ref_solution = ref_solution, bootstrp_solution = bootstrp_solution)
 }
 
-process_principal_components <- function(coef_matrix, pc_labels) {
-  num_components <- nrow(coef_matrix)
-  combined_data <- data.frame()
-  
-  for (i in seq_along(pc_labels)) {
-    lower_bound <- as.data.frame(coef_matrix[, , 1][, i])
-    upper_bound <- as.data.frame(coef_matrix[, , 2][, i])
-    component_data <- cbind(lower_bound, upper_bound, seq(1:num_components))
-    colnames(component_data) <- c("LB", "UB", "Index")
-    component_data$CI_contains_zero <- component_data$LB <= 0 & component_data$UB >= 0
-    component_data$PC <- pc_labels[i]
-    combined_data <- rbind(combined_data, component_data)
-  }
-  
-  combined_data
+# Extract confidence intervals and prepare data for plotting
+extract_and_prepare_ci <- function(bootstrp_solution, coef_type = "xcoef", pc_indexes) {
+  lower <- bootstrp_solution$basic[[coef_type]][, , 1]
+  upper <- bootstrp_solution$basic[[coef_type]][, , 2]
+  num_rows <- nrow(lower)
+  data_frames <- lapply(pc_indexes, function(pc) {
+    LB <- as.data.frame(lower[, pc])
+    UB <- as.data.frame(upper[, pc])
+    data <- cbind(LB, UB, seq(1:num_rows))
+    colnames(data) <- c("LB", "UB", "Index")
+    data$CI_contains_zero <- ifelse(data$LB <= 0 & data$UB >= 0, TRUE, FALSE)
+    data$PC <- paste0("\u03B2", pc)
+    data
+  })
+  do.call(rbind, data_frames)
 }
 
-plot_confidence_intervals <- function(data, file_path, title, ylim_range = NULL) {
+# Plot confidence intervals
+plot_ci <- function(data, title, output_path, y_limits = NULL) {
   plot <- ggplot(data, aes(x = Index, ymin = LB, ymax = UB, color = CI_contains_zero)) +
     geom_errorbar(width = 0.2) +
     geom_point(aes(y = (LB + UB) / 2)) +
     scale_color_manual(values = c("red", "blue")) +
     facet_wrap(~ PC, scales = "free_y", ncol = 1) +
-    ggtitle(title) +
     geom_hline(yintercept = 0, color = "black") +
-    labs(x = "Index", y = "Value", color = "CI Contains Zero") +
+    ggtitle(title) +
+    labs(x = "Principal Component", y = "Value", color = "CI Contains Zero") +
     theme_minimal()
-  
-  # Add ylim if specified
-  if (!is.null(ylim_range)) {
-    plot <- plot + ylim(ylim_range[1], ylim_range[2])
+  if (!is.null(y_limits)) {
+    plot <- plot + ylim(y_limits)
   }
-  
-  ggsave(file_path, plot = plot, width = 10, height = 6)
+  ggsave(output_path, plot = plot, width = 10, height = 6)
 }
 
-
-calculate_loadings <- function(ref_solution, idx1_X, idx2_X, idx1_Y, idx2_Y) {
-  canonical_loadings_X <- ref_solution$scores$corr.X.xscores
-  canonical_loadings_Y <- ref_solution$scores$corr.Y.yscores
-  
-  list(
-    Loadings_X_Direction1 = canonical_loadings_X[idx1_X, 1],
-    Loadings_X_Direction2 = canonical_loadings_X[idx2_X, 2],
-    Loadings_Y_Direction1 = canonical_loadings_Y[idx1_Y, 1],
-    Loadings_Y_Direction2 = canonical_loadings_Y[idx2_Y, 2]
-  )
-}
-
-calculate_contributions <- function(loadings, component_indices) {
-  selected_loadings <- loadings[, component_indices]
+# Calculate percentage contributions for loadings
+calculate_contributions <- function(loadings, indexes) {
+  selected_loadings <- loadings[, indexes]
   squared_sums <- rowSums(selected_loadings^2)
-  percentages <- (squared_sums / sum(loadings^2)) * 100
-  data.frame(Variable = rownames(selected_loadings), Percentage_Contribution = percentages) %>%
-    arrange(desc(Percentage_Contribution))
+  total_sum <- sum(loadings^2)
+  percentage_contributions <- (squared_sums / total_sum) * 100
+  data.frame(Variable = rownames(selected_loadings), Percentage_Contribution = percentage_contributions)
 }
-
